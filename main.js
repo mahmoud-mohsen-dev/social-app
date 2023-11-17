6ca34a66450d5d95b1779d09e79453518796f9f0
mody120 <mahmoud.mohsen120@gmail.com>
2023-11-17 23:17:54 +0200

    changing the import libraries to cdns

--- main.js
+++ main.js
@@ -108,7 +108,9 @@ function createPost(postObj, tagsElements) {
         <div class="card w-75 mx-auto mb-4">
             <div class="card-header">
                 <img src="${profileImage}" class="rounded-circle border border-3 me-2" style="width: 40px; height: 40px;">
-                <b onclick='getUserBtnClick(${postObj.author.id})' style='cursor: pointer;'>${profilenName}</b>
+                <b onclick='getUserBtnClick(${
+                  postObj.author.id
+                })' style='cursor: pointer;'>${profilenName}</b>
                 <button class='btn btn-outline-danger delete-btn' style='float: right; visibility: ${deleteVisibility}' data-bs-toggle="modal" data-bs-target="#delete-modal" onclick='getPostObj(${JSON.stringify(
     postObj
   )})'>Delete</button>
@@ -121,7 +123,7 @@ function createPost(postObj, tagsElements) {
             })" style="cursor: pointer" >
                 <img src="${
                   postObj.image
-                }" class="w-100 object-fit-cover" style="height: 400px;">
+                }" class="w-100 " style="height: 400px;object-fit: cover;">
 
                 <h5 class="text-primary fs-6 mt-2">${postObj.created_at}</h5>
                 <h4>${postObj.title}</h4>
@@ -131,7 +133,7 @@ function createPost(postObj, tagsElements) {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
                     </svg>
-                    <span class="fw-medium">(${
+                    <span style='font-weight: 500'>(${
                       postObj.comments_count
                     }) Comments</span>
                     <span class='tags'>${tagsElements}</span>
